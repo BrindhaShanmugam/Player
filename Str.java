89d121b8523d58d1b454e2e7ba62fa5ac9f98fb8
BrindhaShanmugam <brindhakartheepan@gmail.com>
2016-09-02 12:18:49 +0530

    Add files via upload

---
+++ Str.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Str{
+public static void main(String ag[]){
+Scanner sc=new Scanner(System.in);
+String s1=sc.next();
+int a=Integer.parseInt(s1);
+System.out.println(a);
+}
+}
